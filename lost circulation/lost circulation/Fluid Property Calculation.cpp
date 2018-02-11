113ff718d45dc36618452e65a7408ab40a779ab9
victormaggie <ren_rui@petrochina.com.cn>
2018-02-10 21:48:13 -0600

    Update Fluid Property Calculation.cpp

--- lost circulation/lost circulation/Fluid Property Calculation.cpp
+++ lost circulation/lost circulation/Fluid Property Calculation.cpp
@@ -5,10 +5,5 @@ using namespace std;
 
 double v_av = Q/A;		     // average velocity
 
-int r_s = ((1 + 2 * m) / 3 / m)*(12 * v_av) / (D_po - D_pi);
-double t_w = t_y + pow(r_s, m);
-double x = t_y / t_w;
-double Ca = 1 - x / (1 + m) - m* pow(x, 2) / (1 + m);
-double d = (1 + 2 * m) / (Ca * r_s);
-double t_w1 = t_y + K * pow(d,m);
+
 
